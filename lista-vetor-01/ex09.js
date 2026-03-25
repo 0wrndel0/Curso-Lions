89ece7261795c2dbeecdc636ee6cbc9450c6899d
Wendel  Oliveira <wendelisack2006@gmail.com>
2026-03-25 16:03:20 -0300

    ex09  da lista de vetor

---
+++ lista-vetor-01/ex09.js
@@ -0,0 +1,8 @@
+let numeros = [13, 2, 12, 4, 11, 9, 10, 8];
+let maiores = 0;
+for (let  i= 0; i < numeros.length ; i++){
+    if (numeros [i] > 10){
+   maiores  ++
+    }
+}
+console.log (maiores, "sao maiores")
