dbc939283bcc3e2492327a48fd6095519254c0f9
Wendel  Oliveira <wendelisack2006@gmail.com>
2026-03-26 13:52:36 -0300

    exercicio 15 da lista vetor

---
+++ lista-vetor-01/ex15.js
@@ -0,0 +1,15 @@
+const nums =[1, 2, 3, 4, 5, 6 ]
+let crescente = true;
+
+for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+        crescente = false;
+        break;
+    }
+}
+
+if (crescente) {
+    console.log("Crescente");
+} else {
+    console.log("Não crescente");
+}
